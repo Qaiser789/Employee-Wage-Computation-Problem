ecb67578764dafe68e97587036f8c69c17817ebc
Qaiser789 <sutarqaiser@gmail.com>
2024-01-25 18:12:10 +0530

    Employee Wage calculating functionality added.

--- src/com/bridgelabz/task/EmpWageBuilder.java
+++ src/com/bridgelabz/task/EmpWageBuilder.java
@@ -5,14 +5,22 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		
 		int Is_Full_Time = 1;
+		int Emp_Rate_Per_Hour = 20;
+		
+		// Variables
+		
+		int empHrs = 0;
+		int empWage = 0 ;
 		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
 		if (empCheck == Is_Full_Time) {
-			System.out.println("Employee is Present.");
+			empHrs = 8;
 		}
 		else {
-			System.out.println("Employee is Abscent");
+			empHrs = 0;
 		}
+		empWage = empHrs * Emp_Rate_Per_Hour;
+		System.out.println("Emp Wage: " + empWage);
 
 	}
 
